2ab3840d8e1d0b3929d9e2c7641fee607b7e3fa3
PedroDB25 <pedro-0025@hotmail.com>
2023-10-07 01:13:31 +0200

    cambios estilo

--- static/js/script.js
+++ static/js/script.js
@@ -3,21 +3,39 @@ import recuerdos from "../json/recuerdos.json" assert { type: "json" }
 window.onload = init();
 
 function init() {
-    cagarCartas();
+    cargarCartas();
+    añadirEventos()
 }
 
-function cagarCartas() {
+function añadirEventos() {
+    [...document.querySelectorAll(".cambioSeccion")].forEach(el => el.addEventListener("click", cambiarPagina));
+    let tipo = 1;
+    [...document.querySelectorAll(".SelectorRecuerdos")].forEach(el => el.addEventListener("click", cargarCartas));
+}
+function cambiarPagina() {
+    document.querySelectorAll("section").forEach(el => el.classList.add("hidden"))
+    document.querySelector(`section#${this.id}`).classList.remove("hidden")
+}
+
+function cargarCartas(tipoRecuerdo) {
+    tipoRecuerdo = this?.dataset.recuerdo
+    tipoRecuerdo = tipoRecuerdo ?? 1
     let cajaCartas = document.querySelector("div#catalogo")
+    cajaCartas.innerHTML = ""
     for (const recuerdo of recuerdos) {
+        if (tipoRecuerdo != recuerdo.tipo) { continue }
+
+        let cajaCarta = document.createElement("div")
+        cajaCarta.className = "col-12 col-sm-6 col-md-4 col-lg-3"
 
         let carta = document.createElement("div")
-        carta.className = "card m-3"
-        carta.style.width = "20%"
-        carta.style.backgroundColor = recuerdo.tipo ==1 ? "#f3e2d1" : "#d1dff3"
+        carta.className = "card p-3"
+        carta.style.width = "100%"
+        carta.style.backgroundColor = recuerdo.tipo == 1 ? "#f3e2d1" : "#d1dff3"
         carta.style.border = "0px"
 
         let cartaimg = document.createElement("img")
-        cartaimg.src = "static/img/recuerdos/" + recuerdo.link
+        cartaimg.src = "/wow/static/img/recuerdos/" + recuerdo.link
         cartaimg.className = "card-img-top"
         cartaimg.alt = recuerdo.nombre
 
@@ -30,7 +48,7 @@ function cagarCartas() {
 
         let descripcion = document.createElement("p")
         descripcion.className = "card-text"
-        descripcion.innerHTML = (recuerdo.descripcion.length < 60 ? recuerdo.descripcion : (recuerdo.descripcion.substring(0,57) + "..."))
+        descripcion.innerHTML = (recuerdo.descripcion.length < 60 ? recuerdo.descripcion : (recuerdo.descripcion.substring(0, 57) + "..."))
 
         let precio = document.createElement("p")
         precio.className = "card-text"
@@ -49,7 +67,9 @@ function cagarCartas() {
         carta.appendChild(cartaimg)
         carta.appendChild(cartabody)
 
-        cajaCartas.appendChild(carta)
+        cajaCarta.appendChild(carta)
+
+        cajaCartas.appendChild(cajaCarta)
     }
 
 }
